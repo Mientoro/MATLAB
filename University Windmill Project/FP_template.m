c7218d7be2a0fef1fc96ddf2bcd38dfea6de2919
Peter Erasmus <102836098+Mientoro@users.noreply.github.com>
2022-04-01 17:36:07 -0600

    Add files via upload

---
+++ University Windmill Project/FP_template.m
@@ -0,0 +1,29 @@
+%{
+    Course: ENME 337 - Computing Tools for Engineering Design, Fall 2019
+    Mechanical & Manufacturing Engineering
+    University of Calgary
+    Student:
+
+%}
+
+clear, clc, close all, format short, format compact
+
+%% Loading the data
+
+
+%% User input
+
+
+%% Initialization
+
+
+%% Main Code
+
+% Calculate power for wind speeds between 3m/s and 25m/s using BEM method
+
+
+% Using measured wind speed data compute total energy produced
+
+
+%% Visualization
+
